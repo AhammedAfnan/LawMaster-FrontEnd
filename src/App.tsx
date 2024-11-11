faf4eabfa90ec637e972c9e0209574422e8ce1ae
afnan <afnanballoor44@gmail.com>
2024-11-11 20:20:10 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Welcome to Law Master!
+      </h1>
+    </div>
+  )
+}
+
+export default App
